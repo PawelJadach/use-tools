892ff130cdfa0cb3aefe5854a0d8b3a9090fa698
Pawel Jadach <pawel.jadach@empik.com>
2024-03-19 18:38:44 +0100

    Add simple footer

---
+++ src/ui/Footer.tsx
@@ -0,0 +1,12 @@
+import { maxWidthClass } from "@/config/constants";
+import React from "react";
+
+export default function Footer() {
+	return (
+		<section className={`${maxWidthClass} text-zinc-400 px-4 mx-auto flex flex-wrap items-center justify-center py-8`}>
+			<p className="text-sm text-center">
+				© Copyright 2024, All Rights Reserved by <span className="font-bold">Use-tools</span>
+			</p>
+		</section>
+	);
+}
