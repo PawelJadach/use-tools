bac194c39dc3df1c3acbee941228b782d0d7038e
Pawel Jadach <pawel.jadach@empik.com>
2024-03-19 18:44:28 +0100

    Change category cards classnames

--- src/ui/Cards.tsx
+++ src/ui/Cards.tsx
@@ -52,7 +52,7 @@ export const Cards = ({
 
 export const Card = ({ className, children }: { className?: string; children: React.ReactNode }) => {
 	return (
-		<div className={cn("rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20", className)}>
+		<div className={cn("rounded-2xl h-full w-full p-3 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20", className)}>
 			<div className="relative z-50">
 				<div className="p-4">{children}</div>
 			</div>
@@ -60,10 +60,10 @@ export const Card = ({ className, children }: { className?: string; children: Re
 	);
 };
 export const CardTitle = ({ className, children }: { className?: string; children: React.ReactNode }) => {
-	return <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>{children}</h4>;
+	return <h4 className={cn("text-zinc-100 font-bold tracking-wide", className)}>{children}</h4>;
 };
 export const CardDescription = ({ className, children }: { className?: string; children: React.ReactNode }) => {
-	return <p className={cn("mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm", className)}>{children}</p>;
+	return <p className={cn("mt-4 text-zinc-400 tracking-wide leading-relaxed text-sm", className)}>{children}</p>;
 };
 
 export const CardImage = ({ className, src }: { className?: string; src: string }) => {
