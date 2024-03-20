3072867b364bbc5e5e1984af98398a0184f3113c
Pawel Jadach <pawel.jadach@empik.com>
2024-03-20 17:04:44 +0100

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
 		<>
 			<div className="h-[15rem] md:h-[30rem] w-full flex flex-col text-center items-center justify-center">
 				<h1 className={`text-4xl md:text-7xl font-bold bg-clip-text text-transparent ${gradientClassName}`}>Use-tools</h1>
-				<p className="mt-4 text-neutral-300 max-w-lg">The place where you will find the tools and resources you need as a developer.</p>
+				<p className="mt-4 text-neutral-300 max-w-lg">The place where you will find the tools and resources you need as a developer</p>
 			</div>
 			<div className="max-w-5xl mx-auto px-4">
 				<Cards items={categories} />
