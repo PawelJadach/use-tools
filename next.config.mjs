5b96a9444155177843ff0233decbf64a35041186
Pawel Jadach <pawel.jadach@empik.com>
2024-04-03 19:36:39 +0200

    change next config - fix images

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				domains: "v5.airtableusercontent.com",
-			},
-		],
+		domains: ["v5.airtableusercontent.com"],
 	},
 
 	headers: async () => {
