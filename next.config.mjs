0cdefad563217a55cf77ae1be1643aa50909faed
Pawel Jadach <pawel.jadach@empik.com>
2024-04-03 19:28:29 +0200

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,20 +7,6 @@ const nextConfig = {
 			},
 		],
 	},
-
-	headers: async () => {
-		if (process.env.NODE_ENV !== "production") {
-			return [];
-		}
-
-		return [
-			{
-				source: "/:all*(css|js|gif|svg|jpg|jpeg|png|woff|woff2)",
-				locale: false,
-				headers: [{ key: "Cache-Control", value: "public, max-age=31536000" }],
-			},
-		];
-	},
 };
 
 export default nextConfig;
