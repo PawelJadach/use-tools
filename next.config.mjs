441b55c5b9d19b9b7d529a51c2aa925ca6e21330
Pawel Jadach <pawel.jadach@empik.com>
2024-04-15 11:19:03 +0200

    remove cache

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,6 @@ const nextConfig = {
 	images: {
 		domains: ["v5.airtableusercontent.com"],
 	},
-
-	headers: async () => {
-		if (process.env.NODE_ENV !== "production") {
-			return [];
-		}
-
-		return [
-			{
-				source: "/:all*(css|js|gif|svg|jpg|jpeg|png|woff|woff2)",
-				locale: false,
-				headers: [{ key: "Cache-Control", value: "public, max-age=31536000" }],
-			},
-		];
-	},
 };
 
 export default nextConfig;
